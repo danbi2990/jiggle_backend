943ead9ef7df1c6f758ade500eaf2fa1cd5ebc0d
Jake Park <danbi2990@gmail.com>
2018-02-12 22:26:22 +0900

    Change save script to support Single Bar Chart.

--- models/Template.js
+++ models/Template.js
@@ -5,7 +5,33 @@ const templateSchema = new mongoose.Schema({
   name: String,
   indexToFocus: Array,
   gif: String,
-  shouldFocusMarked: Boolean
+  shouldFocusMarked: Boolean,
+
+  rawData: Array,
+  width_svg: Number,
+  height_svg: Number,
+  margins: {
+    top: Number,
+    bottom: Number,
+    left: Number,
+    right: Number
+  },
+
+  focusType: String,
+  duration: Number,
+  delay: Number,
+  accumedDelay: Number,
+  easing: String,
+  delayType: String,
+
+  backgroundColor: String,
+  paddingBtwRects: Number,
+  radius: Number,
+  unit: String,
+  color: String,
+  colorToFocus: String,
+  opacity: Number,
+  opacityToHide: Number
 });
 
 module.exports = mongoose.model("Template", templateSchema);
